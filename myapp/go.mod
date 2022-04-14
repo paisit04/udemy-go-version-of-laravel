922cf8fb5b01e4dcbc023aa54bbaeba2896492fe
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-13 22:54:55 -0700

    Section 7: Adding support for Postgres to Celeritas

--- myapp/go.mod
+++ myapp/go.mod
@@ -8,10 +8,23 @@ require (
 	github.com/CloudyKit/jet/v6 v6.1.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/paisit04/celeritas v0.0.0-00010101000000-000000000000
+	github.com/upper/db/v4 v4.5.2
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 )
 
 require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
 	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- myapp/routes.go
+++ myapp/routes.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"myapp/data"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,6 +17,68 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handler.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handler.JetPage)
 	a.App.Routes.Get("/sessions", a.Handler.SessionTest)
+
+	a.App.Routes.Get("/users/login", a.Handler.UserLogin)
+	a.App.Routes.Post("/users/login", a.Handler.PostUserLogin)
+	a.App.Routes.Get("/users/logout", a.Handler.Logout)
+
+	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
+		u := data.User{
+			FirstName: "Trevor",
+			LastName:  "Sawler",
+			Email:     "me@here.com",
+			Active:    1,
+			Password:  "password",
+		}
+
+		id, err := a.Models.Users.Insert(u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
+	})
+
+	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
+		users, err := a.Models.Users.GetAll()
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		for _, x := range users {
+			fmt.Fprint(w, x.LastName)
+		}
+	})
+
+	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
+	})
+
+	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		u.LastName = a.App.RandomString(10)
+		err = u.Update(*u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "updated last name to %s", u.LastName)
+	})
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
